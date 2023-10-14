9e9ae38bfb9dc9cdc0796317cc08a0df6d51ef5e
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2023-10-14 18:37:46 -0300

    creo el objeto "nodo" y algunos metodos

--- prueba.py
+++ prueba.py
@@ -1,43 +1,100 @@
-import pandas as pd
+import random
 import requests as rq
 from bs4 import BeautifulSoup
 import re
 
+#clases
+class Nodo():
+    
+    #cuando se incializa un nodo se crea en el grafico, se le da un nombre, la url de su pagina de wikipedia y se le crea una lista con todos los links dentro de ella
+    def __init__(self, nombre) -> None:
+        self.nombre = nombre
+        self.palabras = set()
+        self.parsePage(nombre)
+    
+    #metodo que va a fijarse si dos nodos tienen conexion, osea si uno referencia al otro
+    def checkConn(self, nodo):
+        if self.nombre in nodo.palabras:
+            self.addEdge(nodo)  # Habria que hacer un ToUpper para que el ckeckeo no sea caps sensitive
+     
+    #metodo que me creo una arista con otro nodo que le pase por parametro       
+    def addEdge(self, nodo):
+        pass
+    
+    #metodo que me crea el nodo en un grafico
+    def createNode(self):
+        pass
+    
+    #parsea con bs4 la pagina de wikipedia que le pases, obteniendo los hipervinculos dentro
+    def parsePage(self,nombre):
+        pass        
+        #el prefijo para siguientes busquedas
+        prefijo_wiki = "https://es.wikipedia.org/wiki/"
+        
+        url = prefijo_wiki + nombre
+        #obtengo la pagina de mamiferos
+        page = rq.get(url)
 
-#obtengo la pagina de mamiferos
-page = rq.get("https://es.wikipedia.org/wiki/mammalia")
+        #la parseo en un objeto
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        #armo una lista con los terminos que yo quiero, en este caso todos los links que refertencian a otra pagina dentro de la wikipedia
+        links = soup.find_all('a', attrs={"href": re.compile("^/wiki/")},limit=200)
 
-#la parseo en un objeto
-soup = BeautifulSoup(page.content, "html.parser")
+        #selecciono a partir de que links me va a interesar revisar
+        limite_inferior = 64
 
 
-#armo una lista con los terminos que yo quiero, en este caso todos los links que refertencian a otra pagina dentro de la wikipedia
-links = soup.find_all('a', attrs={"href": re.compile("^/wiki/")},limit=150)
+        #obtengo el texto y lo cargo en el set
+        for i in links[limite_inferior:]:
+            self.palabras.add(i.get_text())
+        
+        #remuevo los resultados vacios
+        try:
+            self.palabras.remove('')
+        except KeyError:
+            pass
+            
 
-#selecciono a partir de que links me va a interesar revisar
-limite_inferior = 64
 
-#lista limpia
-terminos_mamiferos = list()
 
-#obtengo el texto y lo cargo en la lista
-for i in links[limite_inferior:]:
-    terminos_mamiferos.append(i.get_text())
+
+# testeo, tomo una palabra random y veo cuanto tarda en accedr y parsearla 
+
+# lista = []
+# root = Nodo("mamiferos")
+
+# while len(root.palabras) > 0:
+#     nodo = Nodo(random.choice(list(root.palabras)))
+#     if nodo.nombre in lista:
+#         print("REPETIDO----")
+#         continue
+#     else:
+#         lista.append(nodo.nombre)
+#         print(nodo.nombre)
     
 
 
-#el prefijo para siguientes busquedas 
-prefijo_wiki = "https://es.wikipedia.org/wiki/"
     
-#nodo en el centro del grafico, de el va a salir cuatro vertices 
-nodo_cetral = ["mamiferos"]
 
-#4 nodos que estan conectados con mamiferos
-nodos_internos = [terminos_mamiferos[1], terminos_mamiferos[2], terminos_mamiferos[7], terminos_mamiferos[18]]
 
-#lista con nodos totales del grafo
-nodos = nodo_cetral + nodos_internos
 
-#usar sets
 
-print(nodos)
+
+
+
+
+
+    
+# #nodo en el centro del grafico, de el va a salir cuatro vertices 
+# nodo_cetral = ["mamiferos"]
+
+# #4 nodos que estan conectados con mamiferos
+# nodos_internos = [terminos_mamiferos[1], terminos_mamiferos[2], terminos_mamiferos[7], terminos_mamiferos[18]]
+
+# #lista con nodos totales del grafo
+# nodos = nodo_cetral + nodos_internos
+
+# #usar sets
+
+# print(nodos)
